19db5c09aa3ae795c3736b26d419f8b5c36304f3
Kabuki0111 <hikari_to_karasu@hotmail.co.jp>
2015-12-12 22:37:46 +0900

    マネージャークラスにぶら下がってるコントローラークラスがシーン遷移しても消えないかを実験

---
+++ Assets/CubeIQ/Script/Debug/TestAppManger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestAppManger : MonoBehaviour {
+    private float maxTime = 120.0f;
+    private float minTime = 0.0f;
+
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.maxTime -= Time.time;
+        if(this.maxTime <= 0){
+            Debug.Log("test");
+            Application.LoadLevel("main");
+        }
+	}
+}
