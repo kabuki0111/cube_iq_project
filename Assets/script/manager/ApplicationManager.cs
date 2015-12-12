6ab1970d308a5ccfb691d2456548ac30b40f2f63
Kabuki0111 <hikari_to_karasu@hotmail.co.jp>
2015-12-12 21:04:32 +0900

    アプリケーションを総合的に管理するためのメソッドを作成

---
+++ Assets/script/manager/ApplicationManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplicationManager : SingletonMonoBehaviour<ApplicationManager> {
+    //シーンが遷移してもオブジェクトが吐きされないように対応
+    public void Awake(){
+        if(this != Instance){
+            Destroy(this);
+            return;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
